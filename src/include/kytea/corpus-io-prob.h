c35792aa492bf2db8ee2808551f91443b10121f4
Graham Neubig <neubig@gmail.com>
2013-03-21 01:00:21 +0900

    Fixed a big bug in .gitignore

---
+++ src/include/kytea/corpus-io-prob.h
@@ -0,0 +1,23 @@
+#ifndef CORPUS_IO_PROB_H__ 
+#define CORPUS_IO_PROB_H__ 
+
+#include <kytea/corpus-io-full.h>
+
+namespace kytea {
+
+class ProbCorpusIO : public FullCorpusIO {
+
+public:
+    ProbCorpusIO(StringUtil * util, const char* wordBound = " ", const char* tagBound = "/", const char* elemBound = "&", const char* escape = "\\") : FullCorpusIO(util,wordBound,tagBound,elemBound,escape) { allTags_ = true; }
+    ProbCorpusIO(const CorpusIO & c, const char* wordBound = " ", const char* tagBound = "/", const char* elemBound = "&", const char* escape = "\\") : FullCorpusIO(c,wordBound,tagBound,elemBound,escape) { allTags_ = true; }
+    ProbCorpusIO(StringUtil * util, const char* file, bool out, const char* wordBound = " ", const char* tagBound = "/", const char* elemBound = "&", const char* escape = "\\") : FullCorpusIO(util,file,out,wordBound,tagBound,elemBound,escape) { allTags_ = true; } 
+    ProbCorpusIO(StringUtil * util, std::iostream & str, bool out, const char* wordBound = " ", const char* tagBound = "/", const char* elemBound = "&", const char* escape = "\\") : FullCorpusIO(util,str,out,wordBound,tagBound,elemBound,escape) { allTags_ = true; }
+
+    KyteaSentence * readSentence();
+    void writeSentence(const KyteaSentence * sent, double conf = 0.0);
+
+};
+
+}
+
+#endif
---
+++ src/include/kytea/corpus-io-tokenized.h
@@ -0,0 +1,30 @@
+#ifndef CORPUS_IO_TOKENIZED_H__ 
+#define CORPUS_IO_TOKENIZED_H__ 
+
+#include <kytea/corpus-io.h>
+#include <kytea/kytea-string.h>
+
+namespace kytea {
+
+// An IO class for corpora that are tokenized, but with no tags
+class TokenizedCorpusIO : public CorpusIO {
+
+protected:
+
+    bool allTags_;
+    KyteaString bounds_;
+
+public:
+    TokenizedCorpusIO(StringUtil * util, const char* wordBound = " ");
+    TokenizedCorpusIO(const CorpusIO & c, const char* wordBound = " ");
+    TokenizedCorpusIO(StringUtil * util, const char* file, bool out, const char* wordBound = " ");
+    TokenizedCorpusIO(StringUtil * util, std::iostream & str, bool out, const char* wordBound = " ");
+    
+    KyteaSentence * readSentence();
+    void writeSentence(const KyteaSentence * sent, double conf = 0.0);
+
+};
+
+}
+
+#endif
